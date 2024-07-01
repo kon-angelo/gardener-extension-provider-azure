1a42ea512089fef37353f9978f34f5e5f2c8d0be
Konstantinos Angelopoulos <konstantinos.angelopoulos@sap.com>
2024-07-01 13:23:16 +0200

    add failure policy for infra webhook

--- pkg/webhook/infrastructure/add.go
+++ pkg/webhook/infrastructure/add.go
@@ -8,7 +8,9 @@ import (
 	extensionswebhook "github.com/gardener/gardener/extensions/pkg/webhook"
 	v1beta1constants "github.com/gardener/gardener/pkg/apis/core/v1beta1/constants"
 	extensionsv1alpha1 "github.com/gardener/gardener/pkg/apis/extensions/v1alpha1"
+	admissionregistrationv1 "k8s.io/api/admissionregistration/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/utils/ptr"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
@@ -43,12 +45,13 @@ func AddToManager(mgr manager.Manager) (*extensionswebhook.Webhook, error) {
 	logger.Info("Creating webhook")
 	return &extensionswebhook.Webhook{
 		Name:              WebhookName,
-		Target:            extensionswebhook.TargetSeed,
 		Provider:          azure.Type,
+		Path:              webhookPath,
+		Target:            extensionswebhook.TargetSeed,
 		Types:             types,
 		Webhook:           &admission.Webhook{Handler: handler, RecoverPanic: true},
-		Path:              webhookPath,
 		NamespaceSelector: buildSelector(azure.Type),
+		FailurePolicy:     ptr.To(admissionregistrationv1.Fail),
 	}, nil
 }
 
